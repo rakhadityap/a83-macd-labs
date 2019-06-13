8793115ef1710021072331dfb4778ec91f631fc0
Rakha Aditya <rakha_aditya@hotmail.com>
2019-06-13 12:10:22 +0700

    add config

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "macderakhadb.database.windows.net";
+    $user = "rakha";
+    $pass = "Jamblang2019";
+    $db = "macde";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
